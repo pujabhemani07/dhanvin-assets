58a42bc227abd4b792ade387a412301c314685e2
pujabhemani07 <pujabhemani@gmail.com>
2026-09-21 02:10:43 +0530

    Remove incorrect injected header logo site-wide

--- worker/index.js
+++ worker/index.js
@@ -131,7 +131,7 @@ export default {async fetch(request,env){
  if(!contentType.includes('text/html'))return response;
  return new HTMLRewriter()
   .on('head',{element(element){element.append(`<style id="dhanvin-reference-video">${BRAND_CSS}</style>`,{html:true});element.append(`<link rel="stylesheet" href="/css/video-reference.css">`,{html:true});element.append(`<link rel="icon" href="/assets/dhanvin-logo.svg" type="image/svg+xml">`,{html:true})}})
-  .on('header > div > a[href="index.html"]',{element(element){element.setAttribute('class','da-brand-link');element.setAttribute('aria-label','Dhanvin Assets Pvt Ltd home');element.setInnerContent(`<img class="da-brand-logo" src="/assets/dhanvin-logo.svg" alt="Dhanvin Assets Pvt Ltd" width="235" height="64" decoding="async" fetchpriority="high">`,{html:true})}})
+  .on('header > div > a[href="index.html"]',{element(element){element.remove()}})
   .on('body',{element(element){element.append(`<script id="dhanvin-reference-video-js">${BRAND_JS}</script>`,{html:true})}})
   .transform(response);
 }};
